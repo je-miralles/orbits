87da042cf364e92609dfe744699b6760c3189bce
Emilio Miralles <emilio_miralles@sfu.ca>
2023-08-26 01:05:29 +0200

    Add some helper scripts for rendering.

---
+++ r_codes/scripts/knit_fsh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set the first argument as variable file    
+file=$1
+
+filename=$(basename "$file")
+extension="${filename##*.}"
+filename="${filename%.*}"
+
+Rscript -e "library(knitr); knit('$file')" ;
+pandoc ${filename}.md -f markdown -t latex -s -o ./${filename}.tex ;
+pandoc ${filename}.tex -f latex -t html -o ./html/${filename}.html
